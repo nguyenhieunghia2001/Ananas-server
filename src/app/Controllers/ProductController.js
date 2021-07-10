67ac46782b45d6c8d3064dc6122f5235fd159ad4
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-07-10 14:15:40 +0700

    detail product

--- src/app/Controllers/ProductController.js
+++ src/app/Controllers/ProductController.js
@@ -3,12 +3,27 @@ const Product = require("../Models/Product");
 class ProductControler {
   async getAll(req, res, next) {
     try {
-      const products = await Product.find({}).populate(
-        "statuses colors category sizes"
+      const products = await Product.find({}).populate("statuses colors");
+      return res.status(200).json({
+        success: true,
+        products,
+      });
+    } catch (error) {
+      return res.status(404).json({
+        success: false,
+        message: error,
+      });
+    }
+  }
+  async getProductById(req, res, next) {
+    const { id } = req.params;
+    try {
+      const product = await Product.findById(id).populate(
+        "statuses colors category sizes images"
       );
       return res.status(200).json({
         success: true,
-        products
+        product,
       });
     } catch (error) {
       return res.status(404).json({
