2095bf184b1ef6d3dabe1334524814af35aa21f5
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-07-16 20:50:06 +0700

    Update modle account

--- src/app/Models/Account.js
+++ src/app/Models/Account.js
@@ -3,17 +3,22 @@ const Schema = mongoose.Schema;
 
 const Account = new Schema(
   {
-    email: {type: String},
-    password: {type: String},
-    username: {type: String},
-    phone: {type: String},
-    address: {type: String},
-    role: {type: String, default: 'user'},
-    verify: {type: String},
+    email: { type: String },
+    password: { type: String },
+    username: { type: String },
+    phone: { type: String },
+    address: { type: String },
+    role: { type: String, default: "user" },
+    status: {type: Boolean, default: false},
+    verify: { type: String },
+    love: [{
+      product: { type: Schema.Types.ObjectId, ref: "products" },
+      createAt: {type: Date, default: Date.now}
+    }],
   },
   {
     timestamps: true.valueOf,
   }
 );
 
-module.exports = mongoose.model("Account", Account, 'accounts');
+module.exports = mongoose.model("Account", Account, "accounts");
