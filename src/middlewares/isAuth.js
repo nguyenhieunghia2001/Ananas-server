926e1e108abff676bc697342655364dcedc2751b
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-08-21 14:04:02 +0700

    fix isauth

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -2,7 +2,7 @@ const isAuth = (req, res, next) => {
   try {
     const token = req.cookies.access_token;
     if (!token) {
-      return res.status(400);
+      return res.status(422).json({msg: 'token error'});
     }
     next();
   } catch (err) {
