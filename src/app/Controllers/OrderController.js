3fc04820b734d71d0a2a32b5a0f68c3c5e3b9e42
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-08-23 17:11:57 +0700

    [ADMIN] - api revenue order

--- src/app/Controllers/OrderController.js
+++ src/app/Controllers/OrderController.js
@@ -1,5 +1,30 @@
 const Purchase = require("../Models/Purchase");
+const moment = require("moment");
 
+const getAllDate = (startDay, start, end) => {
+  let pos = start;
+  let result = [startDay.format("DD/MM/YYYY")];
+  while (pos < end) {
+    result = [...result, startDay.add(1, "days").format("DD/MM/YYYY")];
+    pos++;
+  }
+  return result;
+};
+const getRevenueByDate = (dates, orders) => {
+  return dates?.map((date) => {
+    return (
+      orders?.reduce((result, item) => {
+        const index = item.status?.findIndex((sta) => sta.name === "2");
+        if (index > -1) {
+          const dayItem = moment(item.status[index].time).format("DD/MM/YYYY");
+          if (date === dayItem && item.status[index].name === "2") {
+            return result + item.totalPrice;
+          }
+        }
+      }, 0) || 0
+    );
+  });
+};
 const populateArr = [
   {
     path: "products.product",
@@ -31,6 +56,43 @@ class PurchaseController {
     return res.status(200).json({
       success: true,
       order,
+    });
+  }
+  async getRevenueDay(req, res) {
+    //giao hàng thành công mới cộng vào doanh thu
+    const orders = await Purchase.find();
+    const dayNow = moment().format("DD/MM/YYYY");
+    const revenue = orders?.reduce((result, item) => {
+      const index = item.status?.findIndex((sta) => sta.name === "2");
+      if (index > -1) {
+        const dayItem = moment(item.status[index].time).format("DD/MM/YYYY");
+        if (dayNow === dayItem && item.status[index].name === "2") {
+          // && item.status.name === '2'
+          return result + item.totalPrice;
+        }
+      }
+    }, 0);
+    return res.status(200).json({
+      success: true,
+      label: 'Doanh thu hôm nay',
+      labels: [moment().format("DD")],
+      data: [revenue] || [],
+    });
+  }
+  async getRevenueMonth(req, res) {
+    //giao hàng thành công mới cộng vào doanh thu
+    const dateTz = moment().tz("Asia/Ho_Chi_Minh");
+    const orders = await Purchase.find();
+    const startWeek = dateTz.startOf("isoWeek");
+
+    const dates = await getAllDate(startWeek, 1, 7);
+    const revenue = await getRevenueByDate(dates, orders);
+
+    return res.status(200).json({
+      success: true,
+      label: 'Doanh thu tuần này',
+      labels: dates,
+      data: revenue || [],
     });
   }
 }
--- src/router/order.js
+++ src/router/order.js
@@ -4,5 +4,7 @@ const orderController = require('../app/Controllers/OrderController')
 
 router.get('/getall', orderController.getAll);
 router.get('/:id', orderController.getOrderById);
+router.get('/revenue/day', orderController.getRevenueDay);
+router.get('/revenue/month', orderController.getRevenueMonth);
 
 module.exports = router;
