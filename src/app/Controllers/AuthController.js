4da0a37402a3101cf66928fffb893b75598163db
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-08-27 07:51:17 +0700

    httpOnly cookie

--- src/app/Controllers/AuthController.js
+++ src/app/Controllers/AuthController.js
@@ -12,7 +12,7 @@ class AuthControler {
     // Và cookie này ở user sẽ không bao giờ đọc được bởi javascript.  Nó sẽ được browser tự động gửi đi khi có yêu cầu.
     res.cookie("access_token", token, {
       maxAge: 365 * 24 * 60 * 60 * 100,
-      //httpOnly: true, // chỉ có http mới đọc được token
+      httpOnly: true, // chỉ có http mới đọc được token
       secure: true, //ssl nếu có, nếu chạy localhost thì comment nó lại
       sameSite: 'none',
     });
