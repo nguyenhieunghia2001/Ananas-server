a1cc2733bd6148598d4d41e061a6b2970e86d5c4
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-08-29 18:37:15 +0700

    fix jwt complete!!

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -4,7 +4,12 @@ const { verifyToken } = require("../service/JsonWebToken");
 const isAuth = async (req, res, next) => {
   try {
     const token = req.cookies.access_token;
-    console.log(req.cookies);
+    if (!token) {
+      return res.status(422).json({
+        success: false,
+        msg: "auth",
+      });
+    }
     const decoded = await verifyToken(token);
     const account = await Account.findOne({ email: decoded.email });
     if (!token || !account) {
